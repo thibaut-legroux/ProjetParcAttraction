8c6f35d179f35f086deaff31982b3af30b2aaaeb
Gueguette <43647133+Gueguette@users.noreply.github.com>
2018-12-06 02:05:08 +0100

    Client modif
    Classe Client modifiée

--- TP5_note/src/Client.java
+++ TP5_note/src/Client.java
@@ -1,24 +1,32 @@
-/**
- * Created by 18004011 on 04/12/18.
- */
-public class Client {
+public class Client extends Thread {
 
     private int nb_client;
-    private Billetterie billetterie;
-
-    public Client(int nb_client){
-        this.nb_client = nb_client;
+    private Billetterie b;
+    private Attraction a1;
+    private Attraction a2;
+    
+    public Client(int nbClient, Billetterie b, Attraction a1, Attraction a2){
+        this.nb_client = nbClient;
+        this.b = b;
+        this.a1=a1;
+        this.a2=a2;
     }
 
+    /** Même fonction que vendre_tickets dans Billetterie
     public void acheter_Ticket(int nb_billet) throws InterruptedException {
         billetterie.vendre_tickets(nb_billet, this);
     }
-
-    public void aller_attraction(int nb_attraction){
-
+    */
+    
+    public void aller_attraction(){
+        //Temps d'aller à l'attraction
     }
-
-    public void faire_attraction(int nb_attraction){
-
+       
+    public void run() {
+        vendre_tickets(nb_tickets, this);
+        aller_attraction();
+        a1.faire_attraction();
+        aller_attraction();
+        a2.faire_attraction();
     }
 }
