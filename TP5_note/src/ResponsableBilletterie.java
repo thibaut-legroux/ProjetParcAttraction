81cbf8641b2600781a050f1d7d39ab323051fc62
Gueguette <43647133+Gueguette@users.noreply.github.com>
2018-12-06 01:35:42 +0100

    ResponsableBilletterie modif
    Classe ResponsableBilletterie  modifiée

--- TP5_note/src/ResponsableBilletterie.java
+++ TP5_note/src/ResponsableBilletterie.java
@@ -1,17 +1,28 @@
-/**
- * Created by 18004011 on 04/12/18.
- */
-public class ResponsableBilletterie {
+public class ResponsableBilletterie extends Thread {
 
-    private boolean occupe;
-    private boolean idle = true;
+    private Billeterie b;
+    private final int ticketsRech = 20;
 
-    public ResponsableBilletterie(int tickets_dispo) {
-
+    public ResponsableBilletterie(Billeterie b) {
+        this.b = b;
+    }
+    
+    public void run() {
+        //Boucle infinie
+        while(true) {
+            try {
+                b.wait();
+            } catch (InterruptedException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+            b.recharger(ticketsRech);
+        }
     }
 
-    public void recharger_tickets(int recharge_tickets) {
+   /** public void recharger_tickets(int recharge_tickets) {
         int tickets_dispo = 20;
         tickets_dispo+=recharge_tickets;
     }
+    */
 }
